035b20b030ef240066302b9a16c08b109558a99a
Zatharv999 <aatharvskadam@gmail.com>
2026-03-04 21:01:43 +0530

    Add files via upload

---
+++ operators1.py
@@ -0,0 +1,7 @@
+a = 98
+b = 94
+c = 41
+d = 96
+e = 11
+f=(98+94+41+96+11)/5
+print(f"the average height of a tree in Jack's front yard : {f}")
---
+++ operators2.py
@@ -0,0 +1,5 @@
+a=int(input("math?"))
+b=int(input("science?"))
+c=int(input("english?"))  
+x=((a+b+c)/300)*100
+print(f"{x} %")   
