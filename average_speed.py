ad425f491b862842490101453846d18409fe678c
Zatharv999 <aatharvskadam@gmail.com>
2026-03-18 21:27:14 +0530

    Add files via upload

---
+++ average_speed.py
@@ -0,0 +1,8 @@
+"""Three cyclists are riding at the speed of 10,20,30 km/h. 
+find the average and compare which cyclist is riding slower than the 
+average speed?"""
+a=10 
+b=20
+c=30
+x=(a+b+c)/3
+print (x)
---
+++ average_speed1.py
@@ -0,0 +1,9 @@
+"""The mean of 40 numbers is 38. Later on, I detected that I misread the 
+number 56 as 36. Find the correct mean of given numbers."""
+average=38
+total_number_of_numbers=40
+sum_of_all_numbers=total_number_of_numbers*average
+original_number=56
+modified_number=36
+new_sum=sum_of_all_numbers+20
+new_average=new_sum/total_number_of_numbers
